75ce5942d6c88c306baba26364c9b45f031b0b2b
Brian Gulotta <brian.gulotta@gmail.com>
2025-09-25 17:28:04 -0500

    custom font implementation

--- src/c/pebbleproj.c
+++ src/c/pebbleproj.c
@@ -2,6 +2,7 @@
 
 static Window *s_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
 
 static void update_time() {
   // Get a tm structure
@@ -23,13 +24,16 @@ static void main_window_load(Window *window){
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_MONTSERRAT_38));
+
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58,52), bounds.size.w, 50));
 
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
@@ -37,6 +41,9 @@ static void main_window_load(Window *window){
 }
 
 static void main_window_unload(Window *window){
+  // Unload GFont
+  fonts_unload_custom_font(s_time_font);
+
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
 }
