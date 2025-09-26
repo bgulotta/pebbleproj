7e939c2b448149d363aa7a645794f6e88d928cc3
Brian Gulotta <brian.gulotta@gmail.com>
2025-09-26 07:20:15 -0500

    restructuring app to make it more modular

--- src/c/windows/main_window.c
+++ src/c/windows/main_window.c
@@ -1,6 +1,6 @@
-#include <pebble.h>
+#include "main_window.h"
 
-static Window *s_window;
+static Window *s_main_window;
 static TextLayer *s_time_layer;
 static GFont s_time_font;
 static TextLayer *s_weather_layer;
@@ -24,7 +24,6 @@ static void update_time() {
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
-
 }
 
 static void battery_update_proc(Layer *layer, GContext *ctx) {
@@ -40,74 +39,6 @@ static void battery_update_proc(Layer *layer, GContext *ctx) {
   // Draw the bar
   graphics_context_set_fill_color(ctx, GColorWhite);
   graphics_fill_rect(ctx, GRect(0, 0, width, bounds.size.h), 0, GCornerNone);
-}
-
-static void main_window_load(Window *window){
-  // Get information about the Window
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
-
-  // Create GBitmap
-  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
-
-  // Create BitmapLayer to display the GBitmap
-  s_background_layer = bitmap_layer_create(bounds);
-
-  // Set the bitmap onto the layer and add to the window
-  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
-
-  // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_MONTSERRAT_48));
-
-  // Create the TextLayer with specific bounds
-  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58,52), bounds.size.w, 50));
-
-  // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_font(s_time_layer, s_time_font);
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
-
-  // Create temperature Layer
-  s_weather_layer = text_layer_create(
-    GRect(0, PBL_IF_ROUND_ELSE(125, 120), bounds.size.w, 25));
-
-  // Style the text
-  text_layer_set_background_color(s_weather_layer, GColorClear);
-  text_layer_set_font(s_weather_layer,fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
-  text_layer_set_text_color(s_weather_layer, GColorWhite);
-  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
-  text_layer_set_text(s_weather_layer, "Loading...");
-
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer,text_layer_get_layer(s_weather_layer));
-
-  // Create battery meter Layer
-  s_battery_layer = layer_create(GRect(14, 54, 115, 2));
-  layer_set_update_proc(s_battery_layer, battery_update_proc);
-
-  // Add to Window
-  layer_add_child(window_get_root_layer(window), s_battery_layer);
-}
-
-static void main_window_unload(Window *window){
-  // Destroy GBitmap
-  gbitmap_destroy(s_background_bitmap);
-
-  // Destroy BitmapLayer
-  bitmap_layer_destroy(s_background_layer);
-
-  // Unload GFont
-  fonts_unload_custom_font(s_time_font);
-
-  // Destroy TextLayer
-  text_layer_destroy(s_time_layer);
-  text_layer_destroy(s_weather_layer);
-  layer_destroy(s_battery_layer);
 }
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
@@ -162,23 +93,94 @@ static void battery_callback(BatteryChargeState state) {
   layer_mark_dirty(s_battery_layer);
 }
 
-static void init() {
-  // Create main Window element and assign to pointer
-  s_window = window_create();
+static void window_load(Window *window) {
+  // Add UI elements here if needed
+  window_set_background_color(window, GColorBlack);
 
-  window_set_background_color(s_window, GColorBlack);
+  // Get information about the Window
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
 
-  // Set handlers to manage the elements inside the Window
-  window_set_window_handlers(s_window, (WindowHandlers){
-    .load = main_window_load,
-    .unload = main_window_unload
-  });
+  // Create GBitmap
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
 
-  // Register with TickTimerService
+  // Create BitmapLayer to display the GBitmap
+  s_background_layer = bitmap_layer_create(bounds);
+
+  // Set the bitmap onto the layer and add to the window
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_MONTSERRAT_48));
+
+  // Create the TextLayer with specific bounds
+  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58,52), bounds.size.w, 50));
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_font(s_time_layer, s_time_font);
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+
+  // Create temperature Layer
+  s_weather_layer = text_layer_create(
+    GRect(0, PBL_IF_ROUND_ELSE(125, 120), bounds.size.w, 25));
+
+  // Style the text
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_font(s_weather_layer,fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text_color(s_weather_layer, GColorWhite);
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_weather_layer, "Loading...");
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer,text_layer_get_layer(s_weather_layer));
+
+  // Create battery meter Layer
+  s_battery_layer = layer_create(GRect(14, 54, 115, 2));
+  layer_set_update_proc(s_battery_layer, battery_update_proc);
+
+  // Add to Window
+  layer_add_child(window_get_root_layer(window), s_battery_layer);
+
+
+}
+
+static void window_unload(Window *window) {
+    // Destroy GBitmap
+    gbitmap_destroy(s_background_bitmap);
+
+    // Destroy BitmapLayer
+    bitmap_layer_destroy(s_background_layer);
+
+    // Unload GFont
+    fonts_unload_custom_font(s_time_font);
+
+    // Destroy TextLayer
+    text_layer_destroy(s_time_layer);
+    text_layer_destroy(s_weather_layer);
+    layer_destroy(s_battery_layer); 
+
+    // Destroy UI elements here if needed
+    window_destroy(s_main_window);    
+}
+
+void main_window_push() {
+    if (!s_main_window) {
+        s_main_window = window_create();
+        window_set_window_handlers(s_main_window, (WindowHandlers) {
+            .load = window_load,
+            .unload = window_unload,
+        });
+    }
+    window_stack_push(s_main_window, true);
+
+    // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-
-  // Show the window on the watch with animated=true
-  window_stack_push(s_window, true);
 
   // Show current time
   update_time();
@@ -199,15 +201,4 @@ static void init() {
 
   // Ensure battery level is displayed from the start
   battery_callback(battery_state_service_peek());
-}
-
-static void deinit() {
-  // Destroy window
-    window_destroy(s_window);
-}
-
-int main(void) {
-  init();
-  app_event_loop();
-  deinit();
 }
